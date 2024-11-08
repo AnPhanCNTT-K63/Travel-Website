60a8285ec2e85e9c827cfa63a485797f49ce3783
AnPhan <126398348+AnPhanCNTT-K63@users.noreply.github.com>
2024-11-08 16:39:25 +0700

    new feat

--- src/api/services.js
+++ src/api/services.js
@@ -1,19 +1,24 @@
 import axios from "axios";
 
-const API_BASE_URL = "https://localhost:44331/api";
+const API_URL = "https://localhost:44331";
 
-// Automatically include token in requests
-axios.interceptors.request.use((config) => {
-  const token = localStorage.getItem("token");
-  if (token) {
-    config.headers.Authorization = `Bearer ${token}`;
-  }
-  return config;
-});
+axios.interceptors.request.use(
+  (config) => {
+    const token = localStorage.getItem("token");
+    if (token) {
+      config.headers["Authorization"] = `Bearer ${token}`;
+    } else {
+      console.log("No token found in localStorage");
+    }
+    console.log("Authorization header:", config.headers["Authorization"]);
+    return config;
+  },
+  (error) => Promise.reject(error)
+);
 
 export const getTours = async () => {
   try {
-    const response = await axios.get(`${API_BASE_URL}/tours`); //https://localhost:44331/api/tours
+    const response = await axios.get(`${API_URL}/Tour/tours`); // GET: /Tour/tours
     return response.data;
   } catch (error) {
     console.error("Error fetching tours:", error);
@@ -23,7 +28,8 @@ export const getTours = async () => {
 
 export const getTourDetail = async (id) => {
   try {
-    const response = await axios.get(`${API_BASE_URL}/tourDetail/?id=${id}`);
+    // console.log(localStorage.getItem("token"));
+    const response = await axios.get(`${API_URL}/Tour/tourDetail/${id}`); //GET: /Tour/tourDetail/{id}
     return response.data;
   } catch (error) {
     console.error("Error fetching tour detail:", error);
@@ -33,7 +39,7 @@ export const getTourDetail = async (id) => {
 
 export const signup = async (user) => {
   try {
-    const res = await axios.post(`${API_BASE_URL}/signup`, user);
+    const res = await axios.post(`${API_URL}/Auth/signup`, user); //POST: /Auth/signup
     return res.data;
   } catch (err) {
     console.log("Error When Fetching Api", err);
@@ -42,7 +48,7 @@ export const signup = async (user) => {
 
 export const signout = async () => {
   try {
-    const res = await axios.get(`${API_BASE_URL}/signout`);
+    const res = await axios.get(`${API_URL}/Auth/signout`); //POST: /Auth/signout
     return res.data;
   } catch (err) {
     console.log("Error When Fetching Api", err);
@@ -51,7 +57,7 @@ export const signout = async () => {
 
 export const signin = async (user) => {
   try {
-    const res = await axios.post(`${API_BASE_URL}/signin`, user);
+    const res = await axios.post(`${API_URL}/Auth/signin`, user); //POST: /Auth/signin
     return res.data;
   } catch (err) {
     console.log("Error When Fetching Api", err);
@@ -60,7 +66,7 @@ export const signin = async (user) => {
 
 export const addTour = async (tourData) => {
   try {
-    const response = await axios.post(`${API_BASE_URL}/tours`, tourData);
+    const response = await axios.post(`${API_URL}/Tour/tourCreate`, tourData);
     return response.data;
   } catch (error) {
     console.error("Error adding tour:", error);
@@ -70,10 +76,7 @@ export const addTour = async (tourData) => {
 
 export const updateTour = async (id, updatedTourData) => {
   try {
-    const response = await axios.put(
-      `${API_BASE_URL}/tours/${id}`,
-      updatedTourData
-    );
+    const response = await axios.put(`${API_URL}/tours/${id}`, updatedTourData);
     return response.data;
   } catch (error) {
     console.error("Error updating tour:", error);
@@ -83,7 +86,7 @@ export const updateTour = async (id, updatedTourData) => {
 
 export const deleteTour = async (id) => {
   try {
-    const response = await axios.delete(`${API_BASE_URL}/tours/${id}`);
+    const response = await axios.delete(`${API_URL}/tours/${id}`);
     return response.data;
   } catch (error) {
     console.error("Error deleting tour:", error);
