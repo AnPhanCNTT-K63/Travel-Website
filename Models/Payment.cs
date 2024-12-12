7985a8ab753de1f29d39bdcd29b4bf7a949d3948
AnPhan <126398348+AnPhanCNTT-K63@users.noreply.github.com>
2024-12-12 08:30:06 +0700

    new feat

--- Models/Payment.cs
+++ Models/Payment.cs
@@ -10,9 +10,7 @@ namespace WebBackendProject.Models
     public class Payment
     {
         public DateTime PaymentDate { get; set; }
-        [Required]
         public string PaymentMethod { get; set; }
-        [Required]
         public float PaymentAmount { get; set; }
         public string PaymentStatus { get; set; }
         public string TransactionId { get; set; }
